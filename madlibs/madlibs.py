a6906eb6a22844c9823d1024300694424c319c66
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-09-10 18:56:25 +0400

    created a variable

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,5 +1,8 @@
-youtuber = "althaf"
+# youtuber = "althaf"
 
-print("subscribe to "+youtuber)
-print("subscribe to {}".format(youtuber))
-print(f"subscribe to {youtuber}")
+# print("subscribe to "+youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+madlib = f"Computer programming is s {adj}!  It makes me so excited all the time because \
+    I love to {verb1}. Stay Hydrated and {verb2}  Like ypu are {famous_person}!"
