a0226a4ca52b1faee89116e4d4cc53724ee4c0a1
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-09-10 18:59:27 +0400

    created input fields

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -4,5 +4,10 @@
 # print("subscribe to {}".format(youtuber))
 # print(f"subscribe to {youtuber}")
 
+adj = input("Adjective:  ")
+verb1 = input("Verb:  ")
+verb2 = input("Verb:  ")
+famous_person  = input("Famous person:  ")
+
 madlib = f"Computer programming is s {adj}!  It makes me so excited all the time because \
     I love to {verb1}. Stay Hydrated and {verb2}  Like ypu are {famous_person}!"
